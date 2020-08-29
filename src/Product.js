8cc8e06db07971fe23587dd2a850d13c35d75ad7
yamashin313 <araiba23@gmail.com>
2020-08-29 11:29:14 +0900

    First commit

---
+++ src/Product.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+class Product extends React.Component {
+  render(){
+    const items = [
+      { id: '1', name: 'diamond', price: '12345'},
+      { id: '2', name: 'chain', price: '6789'}
+    ];
+  
+    const itemList = items.map((item, index) =>
+      <li key={index}><Link to={'/product/'+item.id}>{item.name}</Link></li>
+    );
+    return(
+      <div>
+        <h1>Products</h1>
+        <ul>{itemList}</ul>
+      </div>
+    )
+  }
+}
+
+export default Product;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Top from './Top';
+import Product from './Product';
+
+class RouteList extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Route path='/' component={Top} />
+        <Route path='/products' component={Product} />
+        <Route path='/product/:id' component={Product} />
+      </Router>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouteList />,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
